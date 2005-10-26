9c517a4eafcfd5cd782a496c45ed2aa23ac329a3
rwh <rwh>
2005-10-26 06:13:22 +0000

    Added GPL notice.

--- pymud/game/constants.py
+++ pymud/game/constants.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 """
-$Id: constants.py,v 1.1 2005/05/30 02:00:35 rwh Exp $
-
+$Id: constants.py,v 1.2 2005/10/26 06:14:38 rwh Exp $
 Enums required for Maelstrom
+
+The Pythonic Mud
+Copyright (C) 2005 by Rohan Harris
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
 # Attack enums
--- pymud/utils/misc.py
+++ pymud/utils/misc.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 """
-$Id: misc.py,v 1.1 2005/08/07 07:25:51 rwh Exp $
-
+$Id: misc.py,v 1.2 2005/10/26 06:13:22 rwh Exp $
 Useful functions of randomness.
+
+The Pythonic Mud
+Copyright (C) 2005 by Rohan Harris
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
 def getDict(data, itemDelimiter = ";", valueDelimiter = "="):
